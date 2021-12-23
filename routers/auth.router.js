4d05d22254e427f1eef92c298e1a98d377118c0a
saufiroja <saufiroja08@gmail.com>
2021-12-23 18:17:56 +0800

    membuat validasi login

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 
 const { register, login, test } = require("../controllers/auth.controllers");
+const {
+  registerSchema,
+  loginSchema,
+} = require("../middleware/schema.validation");
 const { authenticationToken } = require("../middleware/jwt.verify");
+const { validate } = require("../middleware/joi.validation");
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 
 router.get("/hello", authenticationToken, test);
 
