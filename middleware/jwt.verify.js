45c64aa192e9142233182a37f55ce022e1077d0b
saufiroja <saufiroja08@gmail.com>
2021-12-24 13:56:59 +0800

    mengubah validate token

--- middleware/jwt.verify.js
+++ middleware/jwt.verify.js
@@ -2,20 +2,27 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { SECRET } = process.env;
 
-const authenticationToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) {
-    return res.sendStatus(401);
-  }
-
-  jwt.verify(token, SECRET, (err, user) => {
-    if (err) {
-      return res.sendStatus(401);
+const authenticationToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const result = await jwt.verify(token, SECRET);
+      req.decoded = result;
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        message: "unauthorized",
+        code: 401,
+        error,
+      });
     }
-    req.user = user;
-    next();
-  });
+  } else {
+    return res.status(401).json({
+      message: "Authentication error. Token required",
+      code: 401,
+    });
+  }
 };
 
 module.exports = { authenticationToken };
